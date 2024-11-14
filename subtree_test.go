2d574d87c1622d927a7cbc8f952f4bd8a76f02b4
Jesse Coretta <{ID}+{username}@users.noreply.github.com>
2024-11-14 04:09:09 -0800

    inaugural push

--- subtree_test.go
+++ subtree_test.go
@@ -10,6 +10,7 @@ func TestSubtreeSpecification(t *testing.T) {
 		`{base "n=1,n=4,n=1,n=6,n=3,n=1", minimum 1, maximum 1, specificationFilter and:{item:1.3.6.1.4.1,or:{item:cn,item:2.5.4.7}}}`,
 		`{base "n=1,n=4,n=1,n=6,n=3,n=1", minimum 1, maximum 1, specificationFilter and:{item:1.3.6.1.4.1,not:item:1.3.6.1.5.5,or:{item:cn,item:2.5.4.7}}}`,
 		`{base "n=1,n=4,n=1,n=6,n=3,n=1", minimum 1, maximum 1, specificationFilter item:1.3.6.1.4.1.56521}`,
+		`{minimum 1, maximum 1}`,
 		`{base "n=1,n=4,n=1,n=6,n=3,n=1", minimum 1, maximum 1, specificationFilter not:item:1.3.6.1.4.1.56521}`,
 		`{base "n=1,n=4,n=1,n=6,n=3,n=1", specificExclusions { chopBefore "n=14", chopAfter "n=555", chopAfter "n=74,n=6" }, minimum 1, maximum 1, specificationFilter item:1.3.6.1.4.1.56521}`,
 		`{}`,
